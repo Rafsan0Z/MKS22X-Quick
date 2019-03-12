be86d756303a114e29ed9781eac624494e564565
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-12 10:01:56 -0400

    Second partitionDutch and quicksort method added!

--- Quick.java
+++ Quick.java
@@ -49,6 +49,14 @@ public class Quick{
     return 0;
   }
 
+  public static int[] partitionDutch(int[] data, int lo, int hi){
+
+  }
+
+  public void quicksort(int[] data){
+    
+  }
+
   public static int median(int num1, int num2, int num3){
     if(num1 < num2 && num2 < num3){return num2;}
     else if(num2 < num1 && num1 < num3){return num1;}
