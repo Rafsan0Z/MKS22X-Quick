9cd3e2515c5a63a8f6fc0ae33a57acd81a8016cd
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-15 20:33:57 -0400

    Insertion Comparison added!

--- KDriver.java
+++ KDriver.java
@@ -2,24 +2,29 @@ import java.util.Arrays;
 public class KDriver{
 
 public static void main(String[]args){
-  System.out.println("Size\t\tMax Value\tquick/builtin ratio\tresult\tArraySort Time\tNormal Partition Time ");
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio\tresult\tArraySort Time\tInsertionSort Time ");
   int[]MAX_LIST = {1000000000,50000,5000,500,50,10};
   double count = 0;
+  double count2 = 0;
   double total = 0;
   for(int MAX : MAX_LIST){
     for(int size = 20000; size < 2000001; size+=100000){
       long qtime=0;
       long btime=0;
       long Qtime = 0;
+      long Itime = 0;
       //average of 5 sorts.
       String result = "";
       String check = "";
+      String Iresult = "";
       for(int trial = 0 ; trial <=10; trial++){
         int []data1 = new int[size];
         int []data2 = new int[size];
+        int []data3 = new int[size];
         for(int i = 0; i < data1.length; i++){
           data1[i] = (int)(Math.random()*MAX);
           data2[i] = data1[i];
+          data3[i] = data1[i];
         }
         long t1,t2,T1,T2;
         t1 = System.currentTimeMillis();
@@ -30,6 +35,10 @@ public static void main(String[]args){
         Arrays.sort(data1);
         t2 = System.currentTimeMillis();
         btime+= t2 - t1;
+        T1 = System.currentTimeMillis();
+        Quick.insertionSort(data3);
+        T2 = System.currentTimeMillis();
+        Itime+= T2 - T1;
         if(!Arrays.equals(data1,data2)){
           System.out.println("FAIL TO SORT!");
           System.exit(0);
@@ -48,7 +57,12 @@ public static void main(String[]args){
         check = "BETTER!";
         count++;
       }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime+"\t"+result+"\t"+check);
+      if(Itime < qtime){Iresult = "WORSE!";}
+      if(qtime <= Itime){
+        Iresult = "BETTER!";
+        count2++;
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime+"\t"+result+"\t"+check+"\t"+Iresult);
       total++;
     }
     System.out.println("ArraySort BETTER %: " + (count/total)*100);
