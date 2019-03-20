61cc6f60a9aaaf892a72eb5e4cafcc9d8f255405
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-19 20:20:30 -0400

    trials increased!

--- KDriver.java
+++ KDriver.java
@@ -1,53 +1,42 @@
 import java.util.Arrays;
+import java.util.Random;
 public class KDriver{
 
-public static void main(String[]args){
-  System.out.println("Size\t\tMax Value\tquick/builtin ratio\tresult");
-  int[]MAX_LIST = {1000000000,5000,500,50};
-  double count = 0;
-  double total = 0;
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=2){
-      long qtime=0;
-      long btime=0;
-      long Qtime = 0;
-      //average of 5 sorts.
-      String result = "";
-      String check = "";
-      String Iresult = "";
-      for(int trial = 0 ; trial <=10; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
+  public static void main(String[]args){
+    Random rn = new Random();
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=10; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Quick.quicksort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
         }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        Quick.quicksort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
       }
-      if(qtime/btime > 3.0){
-        result = "FAIL!";
-      }
-      if(qtime/btime < 3.0){
-        result = "PASS!";
-      }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime+"\t"+result);
-      total++;
+      System.out.println();
     }
-    System.out.println();
   }
-}
 
 
 }
